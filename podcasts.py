b25835f7f0135292ab3f312a0f9cd0e1b30ad847
Malte Bär <maltebaer@posteo.de>
2024-01-15 22:16:32 +0100

    adds volume adjustment

--- podcasts.py
+++ podcasts.py
@@ -1,6 +1,25 @@
 from toniepodcastsync import Podcast, EpisodeSorting
 
-checker_tobi = Podcast("https://feeds.br.de/checkpod-der-podcast-mit-checker-tobi/feed.xml", EpisodeSorting.RANDOM)
-maus_gute_nacht = Podcast("https://kinder.wdr.de/radio/diemaus/audio/gute-nacht-mit-der-maus/diemaus-gute-nacht-104.podcast", EpisodeSorting.RANDOM)
-maus_mix = Podcast("https://kinder.wdr.de/radio/diemaus/audio/diemaus-musik/diemaus-musik-106.podcast", EpisodeSorting.RANDOM)
-maus_zum_hoeren = Podcast("https://kinder.wdr.de/radio/diemaus/audio/diemaus-60/diemaus-60-106.podcast", EpisodeSorting.RANDOM)
+checker_tobi = Podcast(
+    "https://feeds.br.de/checkpod-der-podcast-mit-checker-tobi/feed.xml",
+    episode_sorting = EpisodeSorting.RANDOM,
+    volume_adjustment = 10,
+)
+
+maus_gute_nacht = Podcast(
+    "https://kinder.wdr.de/radio/diemaus/audio/gute-nacht-mit-der-maus/diemaus-gute-nacht-104.podcast",
+    episode_sorting = EpisodeSorting.RANDOM,
+    volume_adjustment = 6,
+)
+
+maus_mix = Podcast(
+    "https://kinder.wdr.de/radio/diemaus/audio/diemaus-musik/diemaus-musik-106.podcast",
+    episode_sorting = EpisodeSorting.RANDOM,
+    volume_adjustment = 6,
+)
+
+maus_zum_hoeren = Podcast(
+    "https://kinder.wdr.de/radio/diemaus/audio/diemaus-60/diemaus-60-106.podcast",
+    episode_sorting = EpisodeSorting.RANDOM,
+    volume_adjustment = 6,
+)
